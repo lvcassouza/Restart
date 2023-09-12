18bb84320ac0866d727b4e3db19dbf9fa84a6478
Lucas Souza <souza.8.lucas@gmail.com>
2023-09-12 13:29:17 -0300

    restart

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Lucas Souza on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
